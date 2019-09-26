05f073d4acd9f4305b7051d9663ad1a1406c5351
tmurphy15 <timothy.murphy10@googlemail.com>
2019-09-26 21:47:58 +0100

    26/09 21:45

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,5 +1,20 @@
 package uk.ac.ed.inf.powergrab;
 
-public class Direction {
-
+public static class Direction {
+	public static final N;
+	public static final Direction S = new Direction();
+	public static final Direction E = new Direction();
+	public static final Direction W = new Direction();
+	public static final Direction NE = new Direction();
+	public static final Direction NW = new Direction();
+	public static final Direction SE = new Direction();
+	public static final Direction SW = new Direction();
+	public static final Direction NNE = new Direction();
+	public static final Direction ENE = new Direction();
+	public static final Direction ESE = new Direction();
+	public static final Direction SSE = new Direction();
+	public static final Direction SSW = new Direction();
+	public static final Direction WSW = new Direction();
+	public static final Direction WNW = new Direction();
+	public static final Direction NNW = new Direction();
 }
--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -1,5 +1,20 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Position {
-
+	public double latitude;
+	public double longitude;
+	public Direction direction = new Direction();
+	
+	public Position(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	public Position nextPosition(Direction direction) {
+		return null;
+	}
+	
+	public boolean inPlayArea() {
+		return true;
+	}
 }
